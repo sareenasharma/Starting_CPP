34d59c5d895d73abdc756246b4116ea0afb0fbe7
Sareena <01.sareena@gmail.com>
2024-12-04 14:41:13 -0800

    Initial commit: Basic input output

---
+++ Basic/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	std::cout << "microsoft visual studio 2022";
+	string name;
+	getline(cin, name);
+	cout << "Name is:" << name << endl;
+	
+	return 0;
+}
